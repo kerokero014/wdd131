d67752e2f8e1bbb7921c3bd2be42a59826d39176
kerokero014 <kevinjrm.kr@gmail.com>
2024-06-17 20:06:57 -0600

    style: Improve recipe card layout and styles

--- recipes/js/main.js
+++ recipes/js/main.js
@@ -4,20 +4,20 @@ import recipes from "./recipes.mjs";
 const createRecipeCard = (recipe) => {
     return `
         <div class="recipe-card">
+        <img src="${recipe.image}" alt="${recipe.name}">
             <div class="recipe__content">
-                <img src="${recipe.image}" alt="${recipe.name}">
                 <h2>${recipe.name}</h2>
+                 <span class="rating" role="img" aria-label="Rating: 4 out of 5 stars">
+                    <span aria-hidden="true" class="icon-star">⭐</span>
+                    <span aria-hidden="true" class="icon-star">⭐</span>
+                    <span aria-hidden="true" class="icon-star">⭐</span>
+                    <span aria-hidden="true" class="icon-star-empty">⭐</span>
+                    <span aria-hidden="true" class="icon-star-empty">☆</span>
+                </span>
                 <div class="recipe__decsription">
                     <p>${recipe.description}</p>
                 </div>
-            
-            <span class="rating" role="img" aria-label="Rating: 4 out of 5 stars">
-                <span aria-hidden="true" class="icon-star">⭐</span>
-                <span aria-hidden="true" class="icon-star">⭐</span>
-                <span aria-hidden="true" class="icon-star">⭐</span>
-                <span aria-hidden="true" class="icon-star-empty">⭐</span>
-                <span aria-hidden="true" class="icon-star-empty">☆</span>
-            </span>
+               
             </div>
         </div>
     `;
